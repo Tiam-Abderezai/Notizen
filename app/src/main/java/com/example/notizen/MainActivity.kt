212bb0cd847150118072bdaf64821f4e7a12c06f
tiamabderezai <pass>
2021-11-18 19:46:00 -0500

    first commit

---
+++ app/src/main/java/com/example/notizen/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.notizen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
