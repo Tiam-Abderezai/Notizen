d3a136e5c9f2fa7f7693715dc66eba9b7ff97513
tiamabderezai <pass>
2021-11-19 00:43:16 -0500

    room classed added

---
+++ app/src/main/java/com/example/notizen/data/NoteRepository.kt
@@ -0,0 +1,31 @@
+package com.example.notizen.data
+
+import android.app.Application
+import com.example.notizen.data.model.Note
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+
+
+class NoteRepository(private val application: Application) {
+    private val noteDao by lazy {
+        NoteDatabase.getDatabase(application).noteDao()
+    }
+
+    fun readAllData() = noteDao.readAllData().flowOn(Dispatchers.IO)
+
+    suspend fun addNote(note: Note) {
+        noteDao.addNote(note)
+    }
+
+    suspend fun updateNote(note: Note) {
+        noteDao.updateNote(note)
+    }
+
+    suspend fun deleteNote(note: Note) {
+        noteDao.deleteNote(note)
+    }
+
+    suspend fun deleteAllNotes() {
+        noteDao.deleteAllNotes()
+    }
+}
---
+++ app/src/main/java/com/example/notizen/data/local/NoteDao.kt
@@ -0,0 +1,23 @@
+package com.example.notizen.data
+import androidx.room.*
+import com.example.notizen.data.model.Note
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Query("SELECT * FROM note_database")
+    fun readAllData(): Flow<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote (note: Note)
+
+    @Query("DELETE FROM note_database")
+    suspend fun deleteAllNotes()
+
+}
