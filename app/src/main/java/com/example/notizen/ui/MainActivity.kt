a69e9fdf8f487dbe9df9df525dcb8112624ff0de
tiamabderezai <pass>
2021-11-22 09:21:40 -0500

    bottom navigation added

--- app/src/main/java/com/example/notizen/ui/MainActivity.kt
+++ app/src/main/java/com/example/notizen/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.notizen.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.PopupMenu
@@ -13,6 +14,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.notizen.R
 import com.example.notizen.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationBarView
@@ -26,38 +28,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        binding.apply {
-//            navController = Navigation.findNavController(navHostFragment)
-//            setupActionBarWithNavController(navController)
-//            bottomNavigation.setOnItemReselectedListener { item ->
-//                when (item.itemId) {
-//                    R.id.item_home -> {
-//                findNavController().navigate(R.id.action_addFragment_to_listFragment)
-//                    }
-//                    R.id.item_profile -> {
-//                        Navigation.findNavController(navHostFragment)
-//                            .navigate(R.id.action_listFragment_to_profileFragment)
-//                        Toast.makeText(applicationContext, "CLICKED", Toast.LENGTH_SHORT).show()
-                        // Respond to navigation item 2 reselection
-//                    }
-//                }
-//        setupSmoothBottomMenu()
-//            }
-//        }
+        setSupportActionBar(binding.toolbar)
+        initNavigation()
+
     }
 
+    private fun initNavigation() {
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHost.navController
+        NavigationUI.setupWithNavController(binding.toolbar, navController)
+        binding.bottomNavigation.setupWithNavController(navController)
 
-//
-//    NavigationBarView.OnItemSelectedListener { item ->
-//        when(item.itemId) {
-//            R.id.item1 -> {
-//                 Respond to navigation item 1 click
-//                true
-//            }
-//            R.id.item2 -> {
-//                 Respond to navigation item 2 click
-//                true
-//            }
-//            else -> false
-//        }
+        binding.toolbar.setNavigationOnClickListener {
+            when (navController.currentDestination?.id) {
+                R.id.listFragment, R.id.profileFragment -> {
+                    if (onBackPressedDispatcher.hasEnabledCallbacks())
+                        onBackPressedDispatcher.onBackPressed()
+                    else
+                        navController.navigateUp()
+                }
+                else -> navController.navigateUp()
+            }
+        }
+
+    }
 }
